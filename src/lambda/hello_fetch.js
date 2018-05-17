5365d39f1b1640e03c321c167f20c940a54dae15
Irene Morente <imorente@gmail.com>
2018-05-16 20:40:49 -0700

    Add fetch example

---
+++ src/lambda/hello_fetch.js
@@ -0,0 +1,16 @@
+/* In this example, we use fetch to get a random joke from an external API */
+
+import fetch from "node-fetch";
+
+const API_ENDPOINT =
+  "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: `${data.setup} ${data.punchline}`
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
