396e5f7cc300de3ffddfdbcc2d724c8be088d8d8
Irene Morente <imorente@gmail.com>
2018-05-20 19:15:58 -0700

    Simplify styles

--- src/lambda/hello_env.js
+++ src/lambda/hello_env.js
@@ -1,6 +1,5 @@
-/* Don’t try to use an environment variable outside the handler – it won’t be
-defined yet. It will work locally, but not when deployed to AWS. */
-
+// Don’t try to use an environment variable outside the handler – it won’t be
+// defined yet. It will work locally, but not when deployed to AWS.
 // const { GREETING } = process.env
 
 exports.handler = async (event, context) => {
--- src/lambda/hello_slack.js
+++ src/lambda/hello_slack.js
@@ -2,17 +2,17 @@ import querystring from "querystring";
 import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
-  /* Only allow POST */
+  // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  /* When the method is POST, the name will no longer be in the event’s
-  queryStringParameters – it’ll be in the event body encoded as a queryString */
+  // When the method is POST, the name will no longer be in the event’s
+  // queryStringParameters – it’ll be in the event body encoded as a queryString
   const params = querystring.parse(event.body);
   const name = params.name || "World";
 
-  /* Send greeting to Slack */
+  // Send greeting to Slack
   return fetch(process.env.SLACK_WEBHOOK_URL, {
     headers: {
       "content-type": "application/json"
