1d8e039f01382d7253bce152ec8d61928b322475
Irene Morente <imorente@gmail.com>
2018-05-30 18:49:44 -0700

    Change build settings

--- webpack.client.js
+++ webpack.client.js
@@ -1,6 +1,14 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve("dist/client")
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
