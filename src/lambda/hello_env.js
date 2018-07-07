1fe951e0a585826a197b75bf5ad60f5cc3b891fc
Irene Morente <imorente@gmail.com>
2018-07-07 14:57:09 -0700

    Try declaring environment variable outside handler

--- src/lambda/hello_env.js
+++ src/lambda/hello_env.js
@@ -1,10 +1,6 @@
-// Don’t try to use an environment variable outside the handler – it won’t be
-// defined yet. It will work locally, but not when deployed to AWS.
-// const { GREETING } = process.env
+const { GREETING } = process.env;
 
 exports.handler = async (event, context) => {
-  const { GREETING } = process.env;
-
   return {
     statusCode: 200,
     body: GREETING
