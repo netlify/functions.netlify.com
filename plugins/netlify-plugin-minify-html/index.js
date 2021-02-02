711897aeed5247b634f0b2fd0f774fed778e202c
David Darnes <me@daviddarnes.com>
2021-02-02 15:42:25 +0000

    Try with options to false

--- plugins/netlify-plugin-minify-html/index.js
+++ plugins/netlify-plugin-minify-html/index.js
@@ -21,7 +21,8 @@ module.exports = {
         compressor: htmlMinifier,
         input: constants.PUBLISH_DIR + "/**/*.html",
         output: "$1.html",
-        replaceInPlace: true
+        replaceInPlace: true,
+        options: false
       });
     } catch (error) {
       utils.build.failPlugin("The Minify HTML plugin failed.", { error });
