3338599b27c3fa980dd39adb3ee4f3bf86689563
David Darnes <me@daviddarnes.com>
2021-02-10 11:46:06 +0000

    Add anchors to markdown level 2 headings

--- .eleventy.js
+++ .eleventy.js
@@ -7,26 +7,40 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const siteConfig = require("./src/_data/site.js");
 const marked = require("marked");
 const prismjs = require("prismjs");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 
-// Markdown filter: Making the syntax highlighting consistent
+// Enable linked headings in md files
+const markdownLib = markdownIt({ html: true }).use(markdownItAnchor, {
+  permalink: true,
+  level: [2],
+  permalinkSymbol: `
+    <svg width="16" height="16" class="icon  icon--link" role="img" aria-labelledby="link--title">
+      <title id="link--title">Anchor link</title>
+      <use xlink:href="#link" fill="CurrentColor"></use>
+    </svg>
+  `,
+});
+
+// Markdown filter: Making the syntax highlighting consistent between md files and imported Readmes
 marked.setOptions({
-  highlight: function(code, lang) {
+  highlight: function (code, lang) {
     if (prismjs.languages[lang]) {
       return prismjs.highlight(code, prismjs.languages[lang], lang);
     } else {
       return code;
     }
-  }
+  },
 });
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Merge default an theme specific tags together
   eleventyConfig.setDataDeepMerge(true);
 
   // Enable Sass usage
   eleventyConfig.addPlugin(pluginSass, {
     watch: "src/assets/css/*",
-    outputDir: "dist/assets/css"
+    outputDir: "dist/assets/css",
   });
 
   // Enable core SEO features
@@ -35,8 +49,8 @@ module.exports = function(eleventyConfig) {
     image: "/assets/images/logos/logo-with-background.png",
     options: {
       twitterCardType: "summary",
-      imageWithBaseUrl: true
-    }
+      imageWithBaseUrl: true,
+    },
   });
 
   // Add schema data
@@ -45,24 +59,24 @@ module.exports = function(eleventyConfig) {
   // Add a sitemap
   eleventyConfig.addPlugin(pluginSitemap, {
     sitemap: {
-      hostname: siteConfig.url
-    }
+      hostname: siteConfig.url,
+    },
   });
 
   // Add syntax highlighting
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
 
   // Date formatting
-  eleventyConfig.addFilter("readableDate", str =>
+  eleventyConfig.addFilter("readableDate", (str) =>
     new Date(str).toLocaleString("en-US", {
       year: "numeric",
       month: "short",
-      day: "numeric"
+      day: "numeric",
     })
   );
 
   // Display array keys and remove undefined tags
-  eleventyConfig.addFilter("keys", obj => Object.keys(obj));
+  eleventyConfig.addFilter("keys", (obj) => Object.keys(obj));
 
   // Exlude items from an array
   eleventyConfig.addFilter("except", (arr = [], ...values) => {
@@ -74,7 +88,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Markdown filter
-  eleventyConfig.addFilter("markdownify", str => marked(str));
+  eleventyConfig.addFilter("markdownify", (str) => marked(str));
 
   // Copy JavaScript, images, functions and Netlify CMS config into dist
   eleventyConfig.addPassthroughCopy("src/assets/images");
@@ -84,15 +98,18 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({
     "node_modules/@yaireo/tagify/dist/tagify.min.js": "assets/js/tagify.min.js",
-    "node_modules/@yaireo/tagify/dist/tagify.css": "assets/css/tagify.css"
+    "node_modules/@yaireo/tagify/dist/tagify.css": "assets/css/tagify.css",
   });
+
+  // Redefined the markdown library to modify behaviour, to add linkable headings
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     dir: {
       input: "src",
-      output: "dist"
+      output: "dist",
     },
     markdownTemplateEngine: "njk",
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
   };
 };
