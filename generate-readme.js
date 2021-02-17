ffa85176f2aa930f7d00a1d4dbe85cfef2432bb1
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-17 20:53:38 +0500

    code update
    I update the generate-readme.js file with best practice that i used to see github largest projects how code should be formated.
    I also update the netlify-lambda from 0.4.0 to 2.0.3.It's working fine.I checked  it on local server.I aslo update  yarn lock  file with new stable depencies and tested it.It's also working fine.

--- generate-readme.js
+++ generate-readme.js
@@ -1,8 +1,8 @@
 /* script for generating docs example table */
-const fs = require('fs')
-const path = require('path')
-const url = require('url')
-const markdownMagic = require('markdown-magic')
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const markdownMagic = require("markdown-magic");
 
 const config = {
   transforms: {
@@ -12,58 +12,63 @@ const config = {
     <!-- AUTO-GENERATED-CONTENT:END -->
      */
     EXAMPLES_TABLE() {
-      const exampleFile = path.join(__dirname, 'examples.json')
-      const examples = JSON.parse(fs.readFileSync(exampleFile, 'utf8'))
+      const exampleFile = path.join(__dirname, "examples.json");
+      const examples = JSON.parse(fs.readFileSync(exampleFile, "utf8"));
 
       // Make table header
-      let md = '| Example | Author |\n'
-      md += '|:-------|:------:|\n'
+      let md = "| Example | Author |\n";
+      md += "|:-------|:------:|\n";
       // Sort alphabetically
-      examples.sort((a, b) => {
-        return a.name < b.name ? -1 : 1
-      }).forEach((data) => { // eslint-disable-line
-        // add table rows
-        const repoUrl = (Array.isArray(data.code)) ? data.code[0] : data.code
-        const link = (data.url) ? ` [view the project](${data.url})` : ''
-        const userName = username(repoUrl)
-        const profileURL = `http://github.com/${userName}`
-        md += `| **[${formatPluginName(data.name)}](${repoUrl})** <br/>`
-        md += ` ${data.description}${link} | [${userName}](${profileURL}) |\n`
-      })
-      return md.replace(/^\s+|\s+$/g, '')
+      examples
+        .sort((a, b) => {
+          return a.name < b.name ? -1 : 1;
+        })
+        .forEach((data) => {
+          // eslint-disable-line
+          // add table rows
+          const repoUrl = Array.isArray(data.code) ? data.code[0] : data.code;
+          const link = data.url ? ` [view the project](${data.url})` : "";
+          const userName = username(repoUrl);
+          const profileURL = `http://github.com/${userName}`;
+          md += `| **[${formatPluginName(data.name)}](${repoUrl})** <br/>`;
+          md += ` ${data.description}${link} | [${userName}](${profileURL}) |\n`;
+        });
+      return md.replace(/^\s+|\s+$/g, "");
     },
   },
-}
+};
 
-
-const markdownPath = path.join(__dirname, 'README.md')
+const markdownPath = path.join(__dirname, "README.md");
 
 // run md-magic
 markdownMagic(markdownPath, config, () => {
-  console.log('Docs updated!')
-})
+  console.log("Docs updated!");
+});
 
 // util functions
 function toTitleCase(str) {
-  return str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())
+  return str.replace(
+    /\w\S*/g,
+    (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  );
 }
 
 function formatPluginName(string) {
-  return toTitleCase(string.replace(/-/g, ' '))
+  return toTitleCase(string.replace(/-/g, " "));
 }
 
 function username(repo) {
   if (!repo) {
-    return null
+    return null;
   }
 
-  const o = url.parse(repo)
-  var urlPath = o.path
+  const o = url.parse(repo);
+  var urlPath = o.path;
 
-  if (urlPath.length && urlPath.charAt(0) === '/') {
-    urlPath = urlPath.slice(1)
+  if (urlPath.length && urlPath.charAt(0) === "/") {
+    urlPath = urlPath.slice(1);
   }
 
-  urlPath = urlPath.split('/')[0]
-  return urlPath
+  urlPath = urlPath.split("/")[0];
+  return urlPath;
 }
